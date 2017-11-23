c9c63e2ccf611ef05d1d6049c2c1f252f1a9d9ae
Alessandro Galli <a.galli85@gmail.com>
2017-11-23 21:44:16 +0100

    some cleaning added material-ui basic layout with simple navbar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'typeface-roboto';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { withStyles } from 'material-ui/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Apply some reset
+const styles = theme => ({
+    '@global': {
+        html: {
+            background: '#ffffff',
+            WebkitFontSmoothing: 'antialiased', // Antialiasing.
+            MozOsxFontSmoothing: 'grayscale', // Antialiasing.
+        },
+        body: {
+            margin: 0,
+        },
+    },
+});
+
+let AppWrapper = props => props.children;
+AppWrapper = withStyles(styles)(AppWrapper);
+
+ReactDOM.render(
+    <AppWrapper>
+        <App />
+    </AppWrapper>
+    ,
+    document.getElementById('root')
+);
 registerServiceWorker();
